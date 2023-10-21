6e2ca550666502dd17302133d2d36a2ee88cc2c1
suhyeoonn <suhyeon1740@gmail.com>
2023-10-21 21:31:35 +0900

    4장 package 설치 실습

---
+++ ch4/sample-package/index.js
@@ -0,0 +1,8 @@
+console.log("require");
+
+module.exports = {
+  add: (a, b) => a + b,
+  sub: (a, b) => a - b,
+  multi: (a, b) => a * b,
+  addivd: (a, b) => a / b,
+};
---
+++ ch4/sample-test/index.js
@@ -0,0 +1,7 @@
+const calc = require("sample-package");
+
+const a = 17;
+const b = 13;
+
+console.log(calc.add(a, b));
+console.log(calc.sub(a, b));
