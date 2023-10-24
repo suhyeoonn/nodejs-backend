d71d257e45a7f849fc7e11f7510df0a715376cd6
suhyeoonn <suhyeon1740@gmail.com>
2023-10-24 22:45:31 +0900

    [ch7] 페이지 UI 만들기

--- ch7/board/app.js
+++ ch7/board/app.js
@@ -7,7 +7,17 @@ app.set("view engine", "handlebars");
 app.set("views", __dirname + "/views");
 
 app.get("/", (req, res) => {
-  res.render("home", { title: "hi", message: "nice to meet you." });
+  res.render("home", { title: "테스트 게시판", message: "nice to meet you." });
+});
+
+app.get("/write", (req, res) => {
+  res.render("write", { title: "테스트 게시판" });
+});
+
+app.get("/detail/:id", async (req, res) => {
+  res.render("detail", {
+    title: "테스트 게시판",
+  });
 });
 
 app.listen(3000);
