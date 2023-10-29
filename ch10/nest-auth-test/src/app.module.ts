2e9296970208c1209891967e77197c38b23f4ebf
suhyeoonn <suhyeon1740@gmail.com>
2023-10-29 21:26:24 +0900

    [ch10] User 모듈 생성

---
+++ ch10/nest-auth-test/src/app.module.ts
@@ -0,0 +1,22 @@
+import { Module } from '@nestjs/common';
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+import { UserModule } from './user/user.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { User } from './user/user.entity';
+
+@Module({
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'nest-auth-test.sqlite',
+      entities: [User],
+      synchronize: true,
+      logging: true,
+    }),
+    UserModule,
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
