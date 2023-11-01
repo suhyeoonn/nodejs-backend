0592fd8afe82343aef949ef4a1706f94a20369e3
suhyeoonn <suhyeon1740@gmail.com>
2023-11-02 00:46:16 +0900

    [ch12] 파일 업로드 및 정적 파일 서비스

---
+++ ch12/nest-file-upload/src/app.controller.ts
@@ -0,0 +1,27 @@
+import {
+  Controller,
+  Get,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { AppService } from './app.service';
+import { FileInterceptor } from '@nestjs/platform-express';
+import { multerOption } from './multer.options';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @Get()
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Post('file-upload')
+  @UseInterceptors(FileInterceptor('file', multerOption))
+  fileUpload(@UploadedFile() file: Express.Multer.File) {
+    console.log(file);
+    return 'File Upload';
+  }
+}
